af18d20ad8b206d305f1b9a751c832f8ec79c95a
SandipD95 <119502356+SandipD95@users.noreply.github.com>
2024-07-09 23:10:14 +0530

    secondCommit

--- src/test/java/TestClasses/LoginTest.java
+++ src/test/java/TestClasses/LoginTest.java
@@ -1,5 +1,14 @@
 package TestClasses;
 
+
+import org.testng.annotations.Test;
+import util.*;
+
 public class LoginTest {
 
+	
+	@Test
+	void verifyTiltle() {
+		System.out.println("codeVerified");
+	}
 }
